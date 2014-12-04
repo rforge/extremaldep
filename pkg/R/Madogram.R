0261b8e14b9031c570486d9e409c993c37427514
simonepadoan <simonepadoan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-12-04 20:50:00 +0000

    new r file
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/extremaldep@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/Madogram.R
@@ -0,0 +1,84 @@
+#######################################################
+### Authors: Giulia Marcon and Simone Padoan        ###
+### Emails: giulia.marcon@phd.unibocconi.it,        ###
+### simone.padoan@unibocconi.it                     ###
+### Institution: Department of Decision Sciences,   ###
+### University Bocconi of Milan                     ###
+### File name: Madogram.r                           ###
+### Description:                                    ###
+### This file enables to compute the multivariate   ###
+### madogram as proposed in Marcon et al. (2014)    ###
+### Last change: 03/12/2014                         ###
+#######################################################
+
+# The routine estimates the Pickands function using
+# the multivariate madogram
+# Multivariate case
+
+madogram <- function(w,data){
+  
+  lmadogram <- function(x,data){
+    sumdata <- dim(data)
+    if(!is.matrix(w)) w <- matrix(w, ncol= sumdata[2])
+    sumw <- dim(w)
+    if (sumw[2] != sumdata[2]) 
+      stop("`x' must be a vector/matrix with `d' elements/columns")
+    
+    ans <- numeric(sumw[1])
+    u <- matrix(data,sumdata[1],sumdata[2])
+
+    for (i in 1:sumw[1]){
+      for (j in 1:sumdata[2]){
+        e <- ecdf(data[,j])
+        u[,j] <- e(data[,j])
+        u[,j] <- u[,j]^(1/w[i,j])  
+      }
+      ma <- apply(u,1,max)
+      me <- apply(u,1,mean)
+      ans[i] <- mean(ma-me)
+    }
+    
+    return(ans)
+  }
+  
+  ans <- lmadogram(x=w,data)
+  sumdata <- dim(data)
+  if(!is.matrix(w)) w <- matrix(w, ncol= sumdata[2])
+  W <- w/(1+w)
+  cst <- apply(W,1,mean)
+  A <- ((cst + ans) / (1 - cst - ans))
+  return(A)
+}
+
+
+##########################################################
+# Bivariate Case
+
+madogram2 <- function(x,data){
+  ans <- lmadogram2(x,data)
+  w <- cbind(x,1-x)  
+  W <- w/(1+w)
+  cst <- apply(W,1,mean)
+  A <- ((cst + ans) / (1 - cst - ans))
+  return(A)
+}
+
+lmadogram2 <- function(x,data){
+  d <- ncol(data)
+  w <- cbind(x,1-x)
+  ans <- numeric(nrow(w))
+  u <- matrix(NA,nrow(data),d)
+  
+  for (i in 1:nrow(w)){
+    for (j in 1:d){
+      e <- ecdf(data[,j])
+      u[,j] <- e(data[,j])
+      u[,j] <- u[,j]^(1/w[i,j])  
+    }
+    ma <- apply(u,1,max)
+    me <- apply(u,1,mean)
+    ans[i] <- mean(ma-me)
+  }
+  
+  return(ans)
+}
