d7e5477ec61e454fa927c301755167441826b681
simonepadoan <simonepadoan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-07-12 10:31:22 +0000

    commit R code
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/extremaldep@40 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/ApproxLikelihood.R
@@ -0,0 +1,65 @@
+################################################################################################
+### Authors: Boris Beranger and Simone Padoan        	 									 ###
+### 																						 ###	
+###	Emails: borisberanger@gmail.com, simone.padoan@unibocconi.it							 ###
+### 																						 ###
+###	Institutions: Department of Decision Sciences, University Bocconi of Milan				 ###
+### School of Mathematics and Statistics, University of New South Wales 					 ###
+### 																						 ###
+### File name: ApproxLikelihood.r              							             	     ###
+### 																						 ###
+### Description:                                  							      		     ###
+### This file provides the Approximate Likelihood estimation 				 				 ###
+### for the Pairwise Beta, Dirichlet, H??sler-Reiss, Asym Logisitic and Extremal-t models. 	 ###
+### Also calculates the standard errors using the Godambe information matrix.				 ###
+### 																						 ###
+### Last change: 11/07/2015                         		  								 ###
+### 																						 ###
+################################################################################################
+
+
+###############################################################################
+## Matrix square root - taken from Stephen Lake 
+## http://www5.biostat.wustl.edu/s-news/s-news-archive/200109/msg00067.html
+###############################################################################
+
+matrix.sqrt <- function(A)
+{
+	if (length(A)==1)
+    return(sqrt(A))
+	sva <- svd(A)
+	if (min(sva$d)>=0)
+    Asqrt <- sva$u %*% diag(sqrt(sva$d)) %*% t(sva$v)
+	else
+    stop("matrix square root is not defined")
+	return(Asqrt)
+}
+
+alik <- function(data, model, parastart, trace=0, sig=3){
+
+	optimfun<-function(para){
+		return(dens(x=data, model=model, par=para, log=TRUE, vectorial=FALSE))
+	}
+	est.para=optim(parastart, optimfun,method="BFGS",control=list(maxit=500, trace=trace, fnscale=-1)) 
+
+	LogLik <- est.para$value # log likelihood
+	param.est <- est.para$par # Parameters estimates
+
+	n <- nrow(data)
+	s <- 0
+	score <- matrix(nrow=n,ncol=length(param.est))
+	for(i in 1:n){
+		stepJ <- function(para){
+			dens(x=data[i,], model=model, par=para, log=TRUE, vectorial=FALSE)
+		}
+		score[i,] = jacobian(stepJ,param.est)
+		s = s + hessian(stepJ,param.est)	
+
+	} 
+	K=var(score); # variability matrix
+	J=-s; # sensitivity matrix
+	TIC = 2*matrix.trace(K %*% solve(J))-2*LogLik; # TIC
+	SE = diag(matrix.sqrt(solve((J %*% solve(K) %*% J)/n ))) # Standard errors
+		
+	return(list(par=round(param.est,sig), LL=round(LogLik,sig), TIC=round(TIC,sig), SE=round(SE,sig) ))
+}
